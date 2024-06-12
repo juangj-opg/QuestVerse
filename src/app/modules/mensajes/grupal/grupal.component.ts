dec7a5fcf2474663f11d2ec619af03380c0ef373
Juan Garrido <juangj1b@gmail.com>
2024-06-12 22:52:16 +0200

    Poblando el JSON con más mensajes para ver como se comporta

--- src/app/modules/mensajes/grupal/grupal.component.ts
+++ src/app/modules/mensajes/grupal/grupal.component.ts
@@ -35,7 +35,15 @@ export class GrupalComponent implements OnInit {
         '-La noche era oscura, ni la luna ni las estrellas se asomaban sobre el pueblo maldito fantasma. Las casas, alguna vez llenas de vida y color, estaban cerradas y opacas. La flores que alguna vez llenaron las calles de colores y felicidad, reposan tristemente sobre el suelo, oscuras y sus pétalos perdidos en el viento. La brisa fría envolvió el cuerpo del hombre encapuchado, un susurro de la muerte directo en su oído. De repente, El pueblo parece ponerse de un color aún más oscuro y tétrico. Un suspiró de muerte resonó sobre alguien ¿Pero quién susurro?...fácil. los fantasmas alguna vez olvidados...-' +
         '<br><br><b>¿Unas palabras al viento y crees que olvidaré lo que me hiciste?</b>' +
         '<br><br>-murmuro antes de que se pudiera terminar el susurró, una sombra se muestra sobre una cafetería totalmente destruida y quemada, una figura delgada, con ropas rotas y sucias de líquidos rojos ajenos. Un cabello alguna vez rosa pastel ahora era un fucsia oscuro y desordenado, un grito de fantasma se escuchaba por todo lake noff provocando que todos los cristales y ventanas se rompieran a la alta frecuencia, mientras los cristales caían la sombra desapareció,  abalanzarse sobre del encapuchado que tal vez fue un momento que un dolor punzante recorrió el brazo de kyro, unos lentes alguna vez redondos decoran el rostro pálido y herido de un ahora ahora adulto. Que un grito resonó otra vez. Pero tenía un pedazo de carne entre sus dientes, el fantasma caníbal arranco un pedazo de carne del brazo de kyrox, el muchacho escupió el pedazo a un lado asqueado de comer algo si quiera relacionado al esqueroso cuerpo de kyrox-' +
-        '<br><br><b>¡¿QUE TE DA EL DERECHO A DAR LA CARA EN ESTE CEMENTERIO?! MI... CEMENTERIO....</b>',
+        '<br><br><b>¡¿QUE TE DA EL DERECHO A DAR LA CARA EN ESTE CEMENTERIO?! MI... CEMENTERIO....</b>' +
+        '<br><br>-reclamo cotton, su voz ya no era callada y sensible, si no horrible y fuerte. Sus cuerdas vocales están desgastadas y desgarradas de tanto gritar. Levantó su mano apuntando hacia él con un odio que brillaba de sus pupilas quemando la pena del alma de kyrox, fingiendo un tono amable y tierno como tenía antes-' +
+        '<br><br>¿Me recuerdas? Soy yo cotton! te conocí en la cafetería cuando era feliz antes de conocerte tu- <b>MALDITO DEVUÉLVEME LA VIDA</b>' +
+        '<br><br>-grito antes de abalanzarse sobre kyrox otra vez sometiendo al mayor y golpear el rostro del contrario sin mostrar piedad alguna sobre él-' +
+        '<br><br><br><b>TU ME QUITASTE MI LIBERTAD! TU ME VISTE MORIR! ¿Y QUE HICISTE? ¡¡¡¡ME ABANDONASTE DE CUALQUIER MODO!!!!</b>' +
+        '<br><br>-grito otra vez usando una fuerza sobre humana para su cuerpo débil para levantar a kyrox y tirarlo contra la cafetería. Entro otra vez aterrizando sobre el abdomen de este para aplastar sus intestinos podridos, quiero que sientas lo que sentí. Pisaba sus intestinos mirando como se retorcía en el suelo otra vez-' +
+        '<br><br><b>Un año 8 meses y 14 días...¿Y AHORA VUELVES? QUERÍAS VER MI MALDITO CADÁVER ¿CIERTO? pues no te daré el placer.... TODOS SE FUERON. Ling murió, la perra de Ivanna me abandono para irse a sus malditos universos alternos, y el vampiro albino?! JA! DIOS SABRÁ SI NO SE QUEMÓ AL SOL!</b>' +
+        '<br><br>-agarro un vaso cercano para romperlo entre sus dedos ignorando el dolor para ver los trozos entre sus dedos para golpear el rostro de kyrox haciéndole retroceder-' +
+        '<br><br>Pudiste haberme salvado....pudiste haberme perdonado...<b>PERO ME DEJASTE A MORIR POR ESTAR PELEANDO CON TU ESTÚPIDA PERRA NOVIA!</b>....yo nunca te importe...tuve que sobrevivir solo...tu fuiste mi última oportunidad y la desaprovechaste...pero cambiaré eso...mereces esto...y mucho más de lo que te voy a dar...te haré sentir el infierno que yo sentí durante 14944 horas.',
     },
     {
       idMessage: 3,
@@ -45,7 +53,13 @@ export class GrupalComponent implements OnInit {
       characterName: 'Kyrox',
       urlCharacterImage: '/assets/iconCharacters/kyrox.png',
       colorCharacter: '#A80C0C',
-      message: '¿Qué?',
+      message:
+        '<b>Miró hacía aquel cielo oscuro, escuchando cierto grito que no conseguía reconocer del todo y poniendo mala cara, mientras los cristales de su alrededor, cada vez se iban rompiendo más y más, hasta que esa cadena de cristales rotos llegó hasta él y notando como todo a su alrededor de oscurecía de más. Miró a los lados alertados, pero, sin llegarse a dar cuenta, ya tenía un agujero en la capa y una mordida considerable en su brazo de alguien que llegó rápidamente hasta él</b>' +
+        '<br><br><b>Dentro de la capa, se puso su mano imbuida en un aura negra donde le había mordido, mientras le escuchaba, sin conseguir reconocer aún que era Cotton, hasta que, él mismamente dijo su nombre, llegando a poner una cara de sorpresa y dejando de intentar regenerarse la mordida que le hizo</b>'+
+        '<br><br>¿C-cotton? ¿Eres t-..!'+
+        '<br><br><b>Notó como se abalanzaba sobre él, dejándose caer e intentando cubrirse la cara, mientras que Cotton, sin piedad alguna, seguía golpeandole y gritándole. Kyrox intentaba argumentar con él mientras aguantaba los golpes de él, pero, a la vez que sentía que realmente se lo merecía por no haber llegado a tiempo. Sabía que había llegado tarde y cada vez iba bajando más los brazos y recibiendo más golpes</b>'+
+        '<br><br>C-cotton.. y-yo no quería.. quise venir y ayud-'+
+        '<br><br>Mira, que me he cansado de copiar y pegar.',
     },
     {
       idMessage: 4,
@@ -55,7 +69,7 @@ export class GrupalComponent implements OnInit {
       characterName: 'Kyrox',
       urlCharacterImage: '/assets/iconCharacters/kyrox.png',
       colorCharacter: '#A80C0C',
-      message: 'Ah, eres tú, Cott',
+      message: 'Yo lo siento Cott por no haber venido y todo eso.',
     },
     {
       idMessage: 5,
@@ -65,7 +79,7 @@ export class GrupalComponent implements OnInit {
       characterName: 'Kyrox',
       urlCharacterImage: '/assets/iconCharacters/kyrox.png',
       colorCharacter: '#A80C0C',
-      message: '¿Qué te cuentas?',
+      message: 'Al menos mírate, no estás taan mal de aspecto, más o menos.',
     },
     {
       idMessage: 6,
@@ -76,10 +90,7 @@ export class GrupalComponent implements OnInit {
       urlCharacterImage: '/assets/iconCharacters/cotton.png',
       colorCharacter: '#781042',
       message:
-        '-La noche era oscura, ni la luna ni las estrellas se asomaban sobre el pueblo maldito fantasma. Las casas, alguna vez llenas de vida y color, estaban cerradas y opacas. La flores que alguna vez llenaron las calles de colores y felicidad, reposan tristemente sobre el suelo, oscuras y sus pétalos perdidos en el viento. La brisa fría envolvió el cuerpo del hombre encapuchado, un susurro de la muerte directo en su oído. De repente, El pueblo parece ponerse de un color aún más oscuro y tétrico. Un suspiró de muerte resonó sobre alguien ¿Pero quién susurro?...fácil. los fantasmas alguna vez olvidados...-' +
-        '<br><br><b>¿Unas palabras al viento y crees que olvidaré lo que me hiciste?</b>' +
-        '<br><br>-murmuro antes de que se pudiera terminar el susurró, una sombra se muestra sobre una cafetería totalmente destruida y quemada, una figura delgada, con ropas rotas y sucias de líquidos rojos ajenos. Un cabello alguna vez rosa pastel ahora era un fucsia oscuro y desordenado, un grito de fantasma se escuchaba por todo lake noff provocando que todos los cristales y ventanas se rompieran a la alta frecuencia, mientras los cristales caían la sombra desapareció,  abalanzarse sobre del encapuchado que tal vez fue un momento que un dolor punzante recorrió el brazo de kyro, unos lentes alguna vez redondos decoran el rostro pálido y herido de un ahora ahora adulto. Que un grito resonó otra vez. Pero tenía un pedazo de carne entre sus dientes, el fantasma caníbal arranco un pedazo de carne del brazo de kyrox, el muchacho escupió el pedazo a un lado asqueado de comer algo si quiera relacionado al esqueroso cuerpo de kyrox-' +
-        '<br><br><b>¡¿QUE TE DA EL DERECHO A DAR LA CARA EN ESTE CEMENTERIO?! MI... CEMENTERIO....</b>',
+        '...¿Qué?',
     },
     {
       idMessage: 7,
@@ -90,10 +101,7 @@ export class GrupalComponent implements OnInit {
       urlCharacterImage: '/assets/iconCharacters/cotton.png',
       colorCharacter: '#781042',
       message:
-        '-La noche era oscura, ni la luna ni las estrellas se asomaban sobre el pueblo maldito fantasma. Las casas, alguna vez llenas de vida y color, estaban cerradas y opacas. La flores que alguna vez llenaron las calles de colores y felicidad, reposan tristemente sobre el suelo, oscuras y sus pétalos perdidos en el viento. La brisa fría envolvió el cuerpo del hombre encapuchado, un susurro de la muerte directo en su oído. De repente, El pueblo parece ponerse de un color aún más oscuro y tétrico. Un suspiró de muerte resonó sobre alguien ¿Pero quién susurro?...fácil. los fantasmas alguna vez olvidados...-' +
-        '<br><br><b>¿Unas palabras al viento y crees que olvidaré lo que me hiciste?</b>' +
-        '<br><br>-murmuro antes de que se pudiera terminar el susurró, una sombra se muestra sobre una cafetería totalmente destruida y quemada, una figura delgada, con ropas rotas y sucias de líquidos rojos ajenos. Un cabello alguna vez rosa pastel ahora era un fucsia oscuro y desordenado, un grito de fantasma se escuchaba por todo lake noff provocando que todos los cristales y ventanas se rompieran a la alta frecuencia, mientras los cristales caían la sombra desapareció,  abalanzarse sobre del encapuchado que tal vez fue un momento que un dolor punzante recorrió el brazo de kyro, unos lentes alguna vez redondos decoran el rostro pálido y herido de un ahora ahora adulto. Que un grito resonó otra vez. Pero tenía un pedazo de carne entre sus dientes, el fantasma caníbal arranco un pedazo de carne del brazo de kyrox, el muchacho escupió el pedazo a un lado asqueado de comer algo si quiera relacionado al esqueroso cuerpo de kyrox-' +
-        '<br><br><b>¡¿QUE TE DA EL DERECHO A DAR LA CARA EN ESTE CEMENTERIO?! MI... CEMENTERIO....</b>',
+        '¿Lo vas a dejar así y ya?',
     },
   ];
 
